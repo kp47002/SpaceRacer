859d52ab9b2ffbf4f91cc9c4c8a1a1b797b982b1
Karlo Pavlović <kp47003@gmail.com>
2019-05-28 01:22:35 +0200

    revert and add 2 lanes

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,40 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	private bool isSpawning;
-	public GameObject cube;
-	public GameObject pannel;
-	// Use this for initialization
-	void Start () {
-		isSpawning = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (!isSpawning)
+    private bool isSpawning;
+    public GameObject cube;
+    public GameObject pannel;
+    // Use this for initialization
+    void Start()
+    {
+        isSpawning = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isSpawning)
         {
             float timer = Random.Range(1, 2);
             Invoke("SpawnObject", timer);
             isSpawning = true;
         }
-		//float timer2 = 5;
-         //   Invoke("SpawnPannel", timer2);
-	}
-	void SpawnObject()
+        //float timer2 = 5;
+        //   Invoke("SpawnPannel", timer2);
+    }
+    void SpawnObject()
     {
         // Code to spanw your Prefab here
-		int lane = Random.Range(-1, 1);
-		Instantiate(cube, new Vector3(-22, 0.4f, lane), Quaternion.identity);
+        int lane = Random.Range(-2, 2);
+        Instantiate(cube, new Vector3(-22, 0.4f, lane), Quaternion.identity);
         isSpawning = false;
     }
-	void SpawnPannel()
+    void SpawnPannel()
     {
         // Code to spanw your Prefab here
-		
-		 Instantiate(pannel, new Vector3(-22, 0.4f, 0), Quaternion.identity);
-		
-        
+
+        Instantiate(pannel, new Vector3(-22, 0.4f, 0), Quaternion.identity);
+
+
     }
 }
--- Assets/PlayerBehaviour.cs
+++ Assets/PlayerBehaviour.cs
@@ -3,42 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class PlayerBehaviour : MonoBehaviour {
+public class PlayerBehaviour : MonoBehaviour
+{
 
-	int lane;
-	bool left=false, right=false;
-	// Use this for initialization
-	void Start () {
-		lane=0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		if (Input.GetKey("right") & right==false)
-		{
-			right=true;
-			if (lane<1)
-			{
-				lane++;
-			}
-		}else if(!Input.GetKey("right"))
-		{
-			right=false;
-		}
-		 if(Input.GetKey("left") & !left)
-		{
-			left=true;
-			if (lane>-1)
-			{
-				lane--;
-			}
-		}else if(!Input.GetKey("left"))
-		{
-			left=false;
-		}
-		transform.position = new Vector3(transform.position.x, transform.position.y, lane);
-		
-	}
-	
+    int lane;
+    bool left = false, right = false;
+    // Use this for initialization
+    void Start()
+    {
+        lane = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("right") & right == false)
+        {
+            right = true;
+            if (lane < 2)
+            {
+                lane++;
+            }
+        }
+        else if (!Input.GetKey("right"))
+        {
+            right = false;
+        }
+        if (Input.GetKey("left") & !left)
+        {
+            left = true;
+            if (lane > -2)
+            {
+                lane--;
+            }
+        }
+        else if (!Input.GetKey("left"))
+        {
+            left = false;
+        }
+        transform.position = new Vector3(transform.position.x, transform.position.y, lane);
+
+    }
+
 }
