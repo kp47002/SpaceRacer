42439d3b6b087c5e626c58d926a3654848980812
stipev <cupkou12@gmail.com>
2019-05-29 17:28:40 +0200

    dodana scena menija i button-i

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public void playGame()
+	{
+	
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		Debug.Log ("Change SCENE!!");
+
+	}
+}
+
+
+
+
